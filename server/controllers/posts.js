66db004a21a25877537129eab1db5e84d5f35932
KMS <epicblue@hanmail.net>
2021-10-31 12:52:21 +0900

    Express Routing과 요청 처리 함수들 controller에 모듈화, Mongoose 스키마 설정 및 모델 생성

---
+++ server/controllers/posts.js
@@ -0,0 +1,25 @@
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const PostMessages = await PostMessage.find();
+
+    res.status(200).json(PostMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+  const post = req.body;
+  // 폼 데이터를 받아온다.
+  const newPost = new PostMessage(post);
+  // 스키마에 적합한 PostMessage document 생성
+  try {
+    await newPost.save();
+    // 실제로 db와 소통하는 부분
+    res.status(200).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
---
+++ server/models/postMessage.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema({
+  title: String,
+  message: String,
+  creator: String,
+  tags: [String],
+  selectedFile: String,
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+const postMessage = mongoose.model("PostMessage", postSchema);
+// PostMessage라는 Model을 postSchema 제약조건을 사용해서 등록.
+
+export default postMessage;
